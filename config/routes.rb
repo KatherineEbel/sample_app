97ef919122c8c2c27721aa1cc34a9b9cd632e38e
Katherine Ebel <kathyebel@me.com>
2022-07-19 14:15:39 -0400

    creating microposts, display feed

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'help', to: 'static_pages#help'
   get 'about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
+  # prevent routing errors when refreshing browser after creating posts
+  get '/microposts', to: 'static_pages#home'
   get 'sessions/new'
   
   get 'signup', to: 'users#new'
