d0d8410c877aa7a8731407f8bb24ba7c34e5c952
Katherine Ebel <kathyebel@me.com>
2022-07-19 14:57:04 -0400

    deleting microposts, test microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :require_current_user, only: :destroy
   def create
     @micropost = current_user&.microposts.build(micropost_params)
     if @micropost.save
@@ -12,12 +13,20 @@ class MicropostsController < ApplicationController
   end
   
   def destroy
-  
+    @micropost.destroy
+    flash[:success] = "Your post was deleted"
+    # referrer will sometimes be nil in tests
+    redirect_back_or_to(root_url, status: :see_other)
   end
   
   private
   
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+  
+  def require_current_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url, status: :see_other if @micropost.nil?
   end
 end
--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -11,4 +11,14 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to login_url
   end
+  
+  test "should redirect destroy for wrong micropost" do
+    login_as(users(:katherine))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(micropost)
+    end
+    assert_response :see_other
+    assert_redirected_to root_url
+  end
 end
